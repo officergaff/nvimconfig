2d5e63d5a762bb09c17541d9e3178b3a16548377
Li Xin Rui <li.xinrui3@gmail.com>
2024-12-02 19:03:37 -0500

    configured lazygit shortcut

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,32 +1,50 @@
 return {
     'akinsho/toggleterm.nvim',
     version = "*",
-    opts = {
-        size = function(term)
-            if term.direction == "horizontal" then
-                return 15
-            elseif term.direction == "vertical" then
-                return vim.o.columns * 0.4
-            end
-        end,
-        open_mapping = [[<c-\>]],
-        shade_terminals = true,
-        direction = 'float',
-        float_opts = {
-            border = 'curved',
-            winblend = 0,
-        },
-        on_open = function(term)
-            -- Terminal keymaps
-            vim.keymap.set('t', '<C-h>', '<cmd>wincmd h<CR>', { buffer = term.bufnr })
-            vim.keymap.set('t', '<C-j>', '<cmd>wincmd j<CR>', { buffer = term.bufnr })
-            vim.keymap.set('t', '<C-k>', '<cmd>wincmd k<CR>', { buffer = term.bufnr })
-            vim.keymap.set('t', '<C-l>', '<cmd>wincmd l<CR>', { buffer = term.bufnr })
-        end,
-    },
-    keys = {
-        { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>",              desc = "Terminal Float" },
-        { "<leader>th", "<cmd>ToggleTerm size=15 direction=horizontal<cr>", desc = "Terminal Horizontal" },
-        { "<leader>tv", "<cmd>ToggleTerm size=60 direction=vertical<cr>",   desc = "Terminal Vertical" },
-    },
+    config = function()
+        local toggleterm = require('toggleterm')
+        local Terminal = require('toggleterm.terminal').Terminal
+
+        -- Create lazygit terminal
+        local lazygit = Terminal:new({
+            cmd = "lazygit",
+            direction = "float",
+            float_opts = { border = "curved" },
+            on_open = function(term)
+                vim.cmd("startinsert!")
+                vim.keymap.set('n', 'q', '<cmd>close<CR>', { buffer = term.bufnr, silent = true })
+            end,
+        })
+
+        -- Terminal configuration
+        toggleterm.setup({
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return 15
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.4
+                end
+            end,
+            open_mapping = [[<c-\>]],
+            direction = 'float',
+            float_opts = { border = 'curved' },
+            on_open = function(term)
+                local opts = { buffer = term.bufnr }
+                vim.keymap.set('t', '<C-h>', [[<cmd>wincmd h<CR>]], opts)
+                vim.keymap.set('t', '<C-j>', [[<cmd>wincmd j<CR>]], opts)
+                vim.keymap.set('t', '<C-k>', [[<cmd>wincmd k<CR>]], opts)
+                vim.keymap.set('t', '<C-l>', [[<cmd>wincmd l<CR>]], opts)
+            end,
+        })
+
+        -- Keymaps
+        local function map(key, cmd, desc)
+            vim.keymap.set('n', key, cmd, { desc = desc, silent = true })
+        end
+
+        map('<leader>tf', '<cmd>ToggleTerm direction=float<cr>', 'Terminal Float')
+        map('<leader>th', '<cmd>ToggleTerm size=15 direction=horizontal<cr>', 'Terminal Horizontal')
+        map('<leader>tv', '<cmd>ToggleTerm size=60 direction=vertical<cr>', 'Terminal Vertical')
+        map('<leader>gg', function() lazygit:toggle() end, 'Lazygit')
+    end,
 }
